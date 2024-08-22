9cfc1124e5f6807b9f980bf35efa1c6d9f8789cd
Sunryeo <elma9700@gmail.com>
2024-08-22 13:33:57 +0900

    initial commit

---
+++ src/test/java/com/example/ormi5finalteam1/Ormi5FinalTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ormi5finalteam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ormi5FinalTeam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
