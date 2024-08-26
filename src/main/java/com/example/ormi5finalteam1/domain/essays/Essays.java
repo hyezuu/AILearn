7389200bc8200cb769c0b977dfca9825e0b524c3
user <hyamster37@naver.com>
2024-08-27 08:55:46 +0900

    feat(essays):entity정의

---
+++ src/main/java/com/example/ormi5finalteam1/domain/essays/Essays.java
@@ -0,0 +1,28 @@
+package com.example.ormi5finalteam1.domain.essays;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "essays")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Essays {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
