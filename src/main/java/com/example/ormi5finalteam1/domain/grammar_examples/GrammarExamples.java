3a81cd39df385c8d11d94a54631ea05156e0f372
Sunryeo <elma9700@gmail.com>
2024-08-28 04:42:04 +0900

    feat: GrammarExamples ing

--- src/main/java/com/example/ormi5finalteam1/domain/grammar_examples/GrammarExamples.java
+++ src/main/java/com/example/ormi5finalteam1/domain/grammar_examples/GrammarExamples.java
@@ -1,3 +1,13 @@
 package com.example.ormi5finalteam1.domain.grammar_examples;
 
-public class GrammarExamples {}
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class GrammarExamples {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
