192b7b42b99b328e899c420981ab7ba013e3ee77
juungmiin <ajmkk7@gmail.com>
2024-08-28 14:26:46 +0900

    feat(Comment): Comment Service 작성

--- src/main/java/com/example/ormi5finalteam1/service/CommentService.java
+++ src/main/java/com/example/ormi5finalteam1/service/CommentService.java
@@ -1,13 +1,14 @@
 package com.example.ormi5finalteam1.service;
 
-import com.example.ormi5finalteam1.domain.comments.Comment;
-import com.example.ormi5finalteam1.domain.comments.dto.CommentDto;
+import com.example.ormi5finalteam1.domain.comments.Comments;
+import com.example.ormi5finalteam1.domain.comments.dto.CommentsDto;
 import com.example.ormi5finalteam1.repository.CommentRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -15,13 +16,26 @@ public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
-    public List<CommentDto> getCommentsByPostId(Long postId) {
-        return commentRepository.findByPostId(postId);
+    public List<CommentsDto> getComments() {
+        return commentRepository.findAll().stream()
+                .map(CommentsDto::fromEntity)
+                .collect(Collectors.toList());
     }
 
+    public CommentsDto createComment(CommentsDto commentDto) {
+        Comments comment = CommentsDto.toEntity(commentDto);
+        comment.setCreatedAt(LocalDateTime.now());
+        Comments savedComment = commentRepository.save(comment);
+        return CommentsDto.fromEntity(savedComment);
+    }
 
-    public void deleteComment(Long commentId) {
-        commentRepository.deleteById(commentId);
+    public boolean deleteComment(Long id) {
+        return commentRepository.findById(id)
+                .map(comment -> {
+                    commentRepository.delete(comment);
+                    return true;
+                })
+                .orElse(false);
     }
 
 }
