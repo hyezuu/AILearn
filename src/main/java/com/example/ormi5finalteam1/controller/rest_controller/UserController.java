edb9d354bdc213c25adce17731eacf43cc47f447
hyezuu <duudu0704@gmail.com>
2024-08-28 14:23:37 +0900

    rename(user): 유저타임리프 컨트롤러 네이밍 수정

--- src/main/java/com/example/ormi5finalteam1/controller/rest_controller/UserController.java
+++ src/main/java/com/example/ormi5finalteam1/controller/rest_controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class UserRestController {
+public class UserController {
 
     private final UserService userService;
 
--- src/main/java/com/example/ormi5finalteam1/controller/thymeleaf_controller/UserInfoController.java
+++ src/main/java/com/example/ormi5finalteam1/controller/thymeleaf_controller/UserInfoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class UserController {
+public class UserInfoController {
 
     private final UserService userService;
 
