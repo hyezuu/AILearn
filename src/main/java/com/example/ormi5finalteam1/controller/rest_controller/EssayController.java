db7f90959197841508ad20425c320c35c1e7cfb1
nigoleto <nigoleto@gmail.com>
2024-08-28 15:45:02 +0900

    feat(essay): essay entity 수정

---
+++ src/main/java/com/example/ormi5finalteam1/controller/rest_controller/EssayController.java
@@ -0,0 +1,11 @@
+package com.example.ormi5finalteam1.controller.rest_controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EssayController {
+
+
+}
---
+++ src/main/java/com/example/ormi5finalteam1/domain/essay/Essay.java
@@ -0,0 +1,30 @@
+package com.example.ormi5finalteam1.domain.essay;
+
+import com.example.ormi5finalteam1.domain.BaseEntity;
+import com.example.ormi5finalteam1.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "essays")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Essay extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String topic;
+
+    @Column(nullable = false)
+    private String content;
+
+}
