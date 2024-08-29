67351fcc069b825be9f6801bc274d6b17a08e3d8
hyezuu <duudu0704@gmail.com>
2024-08-30 02:18:22 +0900

    fix(user): usercontroller check method

---
+++ src/test/java/com/example/ormi5finalteam1/controller/rest_controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.example.ormi5finalteam1.controller.rest_controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+}
