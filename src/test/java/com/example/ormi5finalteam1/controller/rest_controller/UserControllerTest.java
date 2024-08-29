a363f5bf4db8afcbdeead9b3d8c51568155341c9
hyezuu <duudu0704@gmail.com>
2024-08-30 02:48:47 +0900

    test(user): UserController- checkEmail test

--- src/test/java/com/example/ormi5finalteam1/controller/rest_controller/UserControllerTest.java
+++ src/test/java/com/example/ormi5finalteam1/controller/rest_controller/UserControllerTest.java
@@ -1,8 +1,73 @@
 package com.example.ormi5finalteam1.controller.rest_controller;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+import com.example.ormi5finalteam1.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void checkEmail_은_해당_email이_존재할_시_true_를_반환한다() throws Exception {
+        //given
+        when(userService.isDuplicateEmail(anyString())).thenReturn(true);
+        //when
+        ResultActions actions
+            = mockMvc.perform(
+                get("/api/email-duplication")
+                    .param("email", "email")
+                    .accept(MediaType.APPLICATION_JSON));
+        //then
+        actions.andExpect(status().isOk())
+            .andExpect(content().string("true"));
+    }
+
+    @Test
+    void checkEmail_은_해당_email이_존재하지_않을_시_false_를_반환한다() throws Exception {
+        //given
+        when(userService.isDuplicateEmail(anyString())).thenReturn(false);
+        //when
+        ResultActions actions
+            = mockMvc.perform(
+            get("/api/email-duplication")
+                .param("email", "email")
+                .accept(MediaType.APPLICATION_JSON));
+        //then
+        actions.andExpect(status().isOk())
+            .andExpect(content().string("false"));
+    }
+
+    @Test
+    void checkEmail_은_email_파라미터를_누락할_시_400_에러를_반환한다() throws Exception {
+        //given
+        //when
+        ResultActions actions
+            = mockMvc.perform(
+            get("/api/email-duplication")
+                .accept(MediaType.APPLICATION_JSON));
+        //then
+        actions.andExpect(status().isBadRequest());
+    }
 }
