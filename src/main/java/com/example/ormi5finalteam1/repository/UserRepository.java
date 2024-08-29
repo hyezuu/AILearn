a4bbbdf11646a6c262bf34ff5084b6a25e295783
Sunryeo <elma9700@gmail.com>
2024-08-29 22:57:04 +0900

    chore: data.sql

--- src/main/java/com/example/ormi5finalteam1/repository/UserRepository.java
+++ src/main/java/com/example/ormi5finalteam1/repository/UserRepository.java
@@ -4,12 +4,10 @@ import com.example.ormi5finalteam1.domain.user.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   Boolean existsByEmail(String email);
 
   Boolean existsByNickname(String nickname);
-
-  Optional<User> findById(Long id);
 }
