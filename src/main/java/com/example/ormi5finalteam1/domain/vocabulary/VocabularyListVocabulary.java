e372dac363a83a00eda0a896dd5aee2ac009e50b
hyezuu <duudu0704@gmail.com>
2024-08-30 10:22:21 +0900

    feat(vocabulary): 단어장-단어 브릿지 엔티티 생성

---
+++ src/main/java/com/example/ormi5finalteam1/domain/vocabulary/VocabularyListVocabulary.java
@@ -0,0 +1,41 @@
+package com.example.ormi5finalteam1.domain.vocabulary;
+
+import com.example.ormi5finalteam1.domain.BaseEntity;
+import com.example.ormi5finalteam1.domain.Grade;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "vocabulary_list_vocabularies")
+public class VocabularyListVocabulary extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vocabulary_list_id", nullable = false)
+    private VocabularyList vocabularyList;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vocabulary_id", nullable = false)
+    private Vocabulary vocabulary;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+}
