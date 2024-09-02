7a6e19e7ec786e6c5257bbba8db4962abca702ac
juungmiin <ajmkk7@gmail.com>
2024-09-02 16:57:29 +0900

    fix(Post): pagination/soft-delete 구현

--- src/main/java/com/example/ormi5finalteam1/repository/PostRepository.java
+++ src/main/java/com/example/ormi5finalteam1/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.example.ormi5finalteam1.repository;
 
 import com.example.ormi5finalteam1.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PagingAndSortingRepository<Post, Long> {
+    Page<Post> findAllByDeletedAtIsNullOrderByCreatedAtDesc(Pageable pageable);
 }
