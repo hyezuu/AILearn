bbac0dd9259790ce86f40dc1cbe959f387b41976
hyezuu <duudu0704@gmail.com>
2024-09-05 10:39:30 +0900

    fix(user): @validate 어노테이션 추가

--- src/main/java/com/example/ormi5finalteam1/controller/rest_controller/UserController.java
+++ src/main/java/com/example/ormi5finalteam1/controller/rest_controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
