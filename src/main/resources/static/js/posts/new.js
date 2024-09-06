de5f85825bee9681045f8184f0ec01cba8449adb
nigoleto <nigoleto@gmail.com>
2024-09-06 19:43:15 +0900

    feat(page): 게시글 작성 페이지 api연동

---
+++ src/main/resources/static/js/posts/new.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let userId = null;
+
+    // 사용자 정보를 요청하여 userId를 가져옴
+    fetch("/api/me", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'  // 세션 쿠키를 포함하여 요청
+    })
+        .then(response => response.json())
+        .then(data => {
+            userId = data.id;
+        })
+        .catch(error => {
+            window.alert("로그인이 필요합니다.")
+            console.error('Error fetching user data:', error);
+        });
+
+    document.getElementById("post-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const content = document.getElementById("content").value;
+
+        const postData = {
+            userId: userId,  // 가져온 사용자 ID를 사용
+            title: title,
+            content: content
+        };
+
+        fetch("/api/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                // 응답 본문이 있을 경우에만 JSON 파싱
+                return response.text().then(text => {
+                    return text ? JSON.parse(text) : {};
+                });
+            })
+            .then(data => {
+                console.log("Success:", data);
+                window.location.href = "/posts";
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+    });
+});
