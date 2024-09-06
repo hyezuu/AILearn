6868ddf95a447f25c2fd7fc140a7691fc78e7970
nigoleto <nigoleto@gmail.com>
2024-09-07 02:21:15 +0900

    feat(page): 게시글 수정 페이지 api 연동

---
+++ src/main/resources/static/js/posts/edit.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let userId = null;
+    const postId = window.location.pathname.split("/").slice(-2, -1)[0];
+    fetchPostDetails(postId);
+
+    // 사용자 정보를 요청하여 userId를 가져옴
+    fetch("/api/me", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'  // 세션 쿠키를 포함하여 요청
+    })
+        .then(response => response.json())
+        .then(data => {
+            userId = data.id;
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+
+    // 게시글 상세 불러오기
+    function  fetchPostDetails(id) {
+        fetch(`/api/posts/${id}`, {
+            method : 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Received data:", data); // 데이터를 콘솔에 출력해서 확인
+                displayPostDetails(data);
+            })
+            .catch(error => {
+                console.error('Error fetching post details:', error);
+            });
+    }
+
+    function displayPostDetails(post) {
+        document.getElementById("title").value = post.title;
+        document.getElementById("content").value = post.content;
+    }
+
+
+    // 게시글 수정 api 요청
+    document.getElementById("edit-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const content = document.getElementById("content").value;
+
+        const postData = {
+            userId: userId,  // 가져온 사용자 ID를 사용
+            title: title,
+            content: content
+        };
+
+        fetch(`/api/posts/${postId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData),
+            credentials: 'include'  // 세션 쿠키를 포함하여 요청
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                // 응답 본문이 있을 경우에만 JSON 파싱
+                return response.text().then(text => {
+                    return text ? JSON.parse(text) : {};
+                });
+            })
+            .then(data => {
+                console.log("Success:", data);
+                window.location.href = `/posts/${postId}`;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+    })
+
+
+})
