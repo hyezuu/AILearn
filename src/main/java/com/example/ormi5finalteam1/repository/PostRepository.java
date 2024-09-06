6f15628c4710738c70747f204e6c204764f1d732
juungmiin <ajmkk7@gmail.com>
2024-09-06 10:45:32 +0900

    fix(Post): Post 키워드 조회 기능 추가

--- src/main/java/com/example/ormi5finalteam1/repository/PostRepository.java
+++ src/main/java/com/example/ormi5finalteam1/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.example.ormi5finalteam1.repository;
 
 import com.example.ormi5finalteam1.domain.post.Post;
+import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface PostRepository extends JpaRepository<Post, Long>, PagingAndSortingRepository<Post, Long> {
     Page<Post> findAllByDeletedAtIsNullOrderByCreatedAtDesc(Pageable pageable);
     Page<Post> findAllByUserIdAndDeletedAtIsNullOrderByCreatedAtDesc(Long userId, Pageable pageable);
+    Page<Post> findAllByTitleContainingAndDeletedAtIsNullOrderByCreatedAtDesc(String keyword, Pageable pageable);
 }
