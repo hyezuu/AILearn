9a32e6509e1c45c0714143684f18c2a85293f1e0
hyezuu <duudu0704@gmail.com>
2024-09-09 03:15:20 +0900

    test(user): vocabularyListController 테스트 메서드 추가

--- src/test/java/com/example/ormi5finalteam1/controller/rest_controller/VocabularyListControllerTest.java
+++ src/test/java/com/example/ormi5finalteam1/controller/rest_controller/VocabularyListControllerTest.java
@@ -3,6 +3,7 @@ package com.example.ormi5finalteam1.controller.rest_controller;
 import static com.example.ormi5finalteam1.util.TestSecurityContextFactory.authenticatedProvider;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -63,4 +64,21 @@ class VocabularyListControllerTest {
         verify(vocabularyListService).addVocabulary(provider);
         actions.andExpect(status().isOk());
     }
+
+    @Test
+    void deleteVocabulary_는_유저의_단어장을_삭제하는_서비스_메서드를_호출한다() throws Exception {
+        // given
+        Provider provider = new Provider(1L, "test@email.com", "testuser", Role.USER, Grade.A1, 10);
+        long vocabularyId = 1L;
+        // when
+        ResultActions actions = mockMvc.perform(
+            delete("/api/vocabulary-list/me/vocabularies/{id}", vocabularyId)
+                .with(csrf())
+                .with(authenticatedProvider(provider))
+        );
+        // then
+        verify(vocabularyListService).delete(provider, vocabularyId);
+        actions.andExpect(status().isOk());
+    }
+
 }
