f8cff2e89bd6574aca9277eeb82229884870178a
Sunryeo <elma9700@gmail.com>
2024-09-10 16:31:25 +0900

    fix: update grammar examples info template

--- src/main/java/com/example/ormi5finalteam1/controller/thymeleaf_controller/GrammarExamplesInfoController.java
+++ src/main/java/com/example/ormi5finalteam1/controller/thymeleaf_controller/GrammarExamplesInfoController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class grammerExamplesInfoController {
+public class GrammarExamplesInfoController {
 
     @GetMapping("/grammar-examples")
-    public String getGrammerExamplesInfo() {
+    public String getGrammarExamplesInfo() {
         return "grammar-examples/index";
     }
 }
