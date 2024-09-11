ccad4e3b4c99c83d4b1bfcd647d1b6141be269c8
nigoleto <nigoleto@gmail.com>
2024-09-12 00:02:17 +0900

    feat(page): header 토글 화살표 이미지로 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,42 @@
+// $(document).ready(function() {
+//     // 초기에 nav 메뉴를 숨김
+//     $('nav').hide()
+//     // .mainMenu 클릭 시 토글 기능을 구현
+//     $('.mainMenu').click(function (e) {
+//         e.preventDefault(); // 기본 링크 동작을 방지
+//         $('nav').slideToggle(300); // 300ms 동안 슬라이드 효과로 토글
+//         // 메뉴 텍스트를 토글 (선택사항)
+//         var menuText = $(this).text();
+//         $(this).text(menuText === 'MENU v' ? 'MENU ^' : 'MENU v');
+//     })
+//     // nav 영역 외부 클릭 시 메뉴를 닫기 (선택사항)
+//     $(document).click(function (e) {
+//         if (!$(e.target).closest('header').length) {
+//             $('nav').slideUp(300);
+//             $('.mainMenu').text('MENU v');
+//         }
+//     });
+// });
 $(document).ready(function() {
     // 초기에 nav 메뉴를 숨김
-    $('nav').hide()
+    $('nav').hide();
+
     // .mainMenu 클릭 시 토글 기능을 구현
     $('.mainMenu').click(function (e) {
         e.preventDefault(); // 기본 링크 동작을 방지
         $('nav').slideToggle(300); // 300ms 동안 슬라이드 효과로 토글
-        // 메뉴 텍스트를 토글 (선택사항)
-        var menuText = $(this).text();
-        $(this).text(menuText === 'MENU v' ? 'MENU ^' : 'MENU v');
-    })
+
+        // 이미지 소스를 토글
+        var img = $(this).find('.menu-img');
+        var imgSrc = img.attr('src');
+        img.attr('src', imgSrc === '/images/menu-v.png' ? '/images/menu-up.png' : '/images/menu-v.png');
+    });
+
     // nav 영역 외부 클릭 시 메뉴를 닫기 (선택사항)
     $(document).click(function (e) {
         if (!$(e.target).closest('header').length) {
             $('nav').slideUp(300);
-            $('.mainMenu').text('MENU v');
+            $('.mainMenu img').attr('src', '/images/menu-v.png');
         }
     });
 });
