722a7b27a451e6a105894c69276e8b3d4b73f9d4
hyezuu <duudu0704@gmail.com>
2024-09-16 13:13:17 +0900

    feat(game): 랭킹기능 추가

--- src/main/java/com/example/ormi5finalteam1/controller/rest_controller/GameController.java
+++ src/main/java/com/example/ormi5finalteam1/controller/rest_controller/GameController.java
@@ -1,9 +1,11 @@
 package com.example.ormi5finalteam1.controller.rest_controller;
 
 import com.example.ormi5finalteam1.domain.user.Provider;
+import com.example.ormi5finalteam1.domain.user.dto.GetTop5UserByScore;
 import com.example.ormi5finalteam1.domain.vocabulary.Vocabulary;
 import com.example.ormi5finalteam1.domain.vocabulary.dto.MyScoreDto;
 import com.example.ormi5finalteam1.service.GameService;
+import com.example.ormi5finalteam1.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
 
     private final GameService gameService;
+    private final UserService userService;
 
     @PostMapping("/score")
     public void saveScore(@AuthenticationPrincipal Provider provider, @RequestBody
@@ -30,5 +33,10 @@ public class GameController {
     @GetMapping("/words")
     public List<Vocabulary> getWords(@RequestParam(defaultValue = "10") int count) {
         return gameService.getRandomWords(count);
+    }
+
+    @GetMapping("/rankings")
+    public List<GetTop5UserByScore> getTop5Rankings() {
+        return userService.getTop5UserByScore();
     }
 }
